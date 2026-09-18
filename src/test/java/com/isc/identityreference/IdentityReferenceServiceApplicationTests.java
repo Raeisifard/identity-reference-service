cb6bdac65345565ec26172fa6f8831947dcc553c
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-18 23:23:24 +0330

    feat: implement foundation skeleton

---
+++ src/test/java/com/isc/identityreference/IdentityReferenceServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.isc.identityreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReferenceServiceApplicationTests {
+    @Test void contextLoads() {}
+}
