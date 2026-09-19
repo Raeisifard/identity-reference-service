1a671e27c3f66f0e4e538c7b02e6f2dc19991770
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-19 20:17:06 +0330

    feat: enable observability configuration properties

--- src/main/java/com/isc/identityreference/security/ApiSecurityConfiguration.java
+++ src/main/java/com/isc/identityreference/security/ApiSecurityConfiguration.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@EnableConfigurationProperties({ApiSecurityProperties.class, ApiRateLimitProperties.class})
+@EnableConfigurationProperties({ApiSecurityProperties.class, ApiRateLimitProperties.class, com.isc.identityreference.observability.ObservabilityProperties.class})
 public class ApiSecurityConfiguration {
 
     @Bean
