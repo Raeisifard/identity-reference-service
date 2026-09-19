77d34c9d0a87eda8f8169bd69a49dbbf0d0f0c7c
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-19 20:18:14 +0330

    fix: order request logging after correlation filter

--- src/main/java/com/isc/identityreference/observability/RequestLoggingFilter.java
+++ src/main/java/com/isc/identityreference/observability/RequestLoggingFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 @Component
+@Order(100)
 public class RequestLoggingFilter extends OncePerRequestFilter {
     private static final Logger log = LogManager.getLogger(RequestLoggingFilter.class);
     private final ObservabilityProperties properties;
