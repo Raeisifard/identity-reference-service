9f7bd29eb0de980a96b811fb5335fd0a01f04f05
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-20 17:46:06 +0330

    feat: register admin console configuration

---
+++ src/main/java/com/isc/identityreference/admin/AdminConsoleConfiguration.java
@@ -0,0 +1,8 @@
+package com.isc.identityreference.admin;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(AdminConsoleProperties.class)
+public class AdminConsoleConfiguration {}
