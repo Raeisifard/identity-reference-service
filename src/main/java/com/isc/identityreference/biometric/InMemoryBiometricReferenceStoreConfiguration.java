2532fce3972bb23ff6a7066ab0eb0d0e1fe893b0
Raeisifard <behnam.raeisifard@gmail.com>
2026-09-20 19:48:29 +0330

    fix: disable in-memory biometric store when Oracle persistence is enabled

--- src/main/java/com/isc/identityreference/biometric/InMemoryBiometricReferenceStoreConfiguration.java
+++ src/main/java/com/isc/identityreference/biometric/InMemoryBiometricReferenceStoreConfiguration.java
@@ -2,14 +2,14 @@ package com.isc.identityreference.biometric;
 
 import com.isc.identityreference.application.BiometricReferenceStore;
 import com.isc.identityreference.application.InMemoryBiometricReferenceStore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InMemoryBiometricReferenceStoreConfiguration {
     @Bean
-    @ConditionalOnMissingBean(BiometricReferenceStore.class)
+    @ConditionalOnProperty(prefix = "identity-reference.persistence.oracle", name = "enabled", havingValue = "false", matchIfMissing = true)
     BiometricReferenceStore biometricReferenceStore() {
         return new InMemoryBiometricReferenceStore();
     }
